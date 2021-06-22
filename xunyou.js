b02326a06dda9b8d0a3e0f1f88c02829b8b73f99
xubo5200 <8413951@qq.com>
2021-06-22 10:43:59 +0800

    1

--- xunyou.js
+++ xunyou.js
@@ -10,7 +10,16 @@ https://api.xunyou.mobi/apis/v1/android/session/.*./refresh?.* url script-reques
 
 const magger = '迅游加速器'
 const $ = Env(magger)
-
+let body = $response.body;
+try {
+    $.log(body)
+    body = JSON.parse(body)
+    body = JSON.stringify(body)
+} catch (e) {
+    console.log(e)
+} finally {
+    $done({ body })
+}
 
 const notify = $.isNode() ? require('./sendNotify') : '';
 let xyjsqheader = $.getdata('xyjsqheader')
