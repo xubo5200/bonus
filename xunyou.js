9aae340b1ac59f7584e4b8d89c41d646d9952c9c
xubo5200 <8413951@qq.com>
2021-06-25 17:01:04 +0800

    1

--- xunyou.js
+++ xunyou.js
@@ -125,7 +125,7 @@ function startTask() {
                 // $.log("data:"+data)
                 const result = JSON.parse(data)
 
-                for (const element of result.taskList) {
+                a:for (const element of result.taskList) {
 
                     $.log(element.taskName + "\n")
 
@@ -136,11 +136,11 @@ function startTask() {
                                 if (element.checkPoints['6']) {
                                     await doTask(element.taskId, '6')
                                     $.log("看视频　＋2天VIP,休息" + element.checkPoints['6']/1000 + "秒")  // 每项对应数值；
-                                    return
                                     // await $.wait(element.checkPoints['6'])
                                 } else if (element.checkPoints['3']) {
                                     await doTask(element.taskId, '3')
                                     $.log("看视频　＋1天VIP,休息" + element.checkPoints['3']/1000 + "秒")  // 每项对应数值；
+                                    break a;
                                     // await $.wait(element.checkPoints['3'])
                                 }
                                 // keys.map(key => {
