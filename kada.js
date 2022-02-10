e8590da7d03d08bb1708d99cccf45e02233ad782
xubo5200 <8413951@qq.com>
2022-02-10 13:59:59 +0800

    1

--- kada.js
+++ kada.js
@@ -1,7 +1,19 @@
 /*
 [rewrite_local]
-^https:\/\/service\.hhdd\.com\/story2\/getCollectItems.json url script-response-body https://gitee.com/lxk0301/jd_scripts/raw/master/jd_petTreasureBox.js
+^https:\/\/service\.hhdd\.com\/story2\/getCollectItems.json url script-response-body https://gitee.com/xubo5200/bonus/raw/master/kada.js
 
 https://service.hhdd.com/story2/getCollectItems.json?collectId=81501
 
  */
+
+var body = $response.body;
+var obj = JSON.parse(body);
+if (obj.code == 200) {
+    var arr = obj.data.items;
+    arr.forEach((item) => {
+        if(item.extFlag != 258){
+            item.extFlag =258;
+            console.log("修改成功")
+        }
+    });
+}
