59662c9042671876b3eb014a7105c58d47f92375
xubo5200 <8413951@qq.com>
2022-02-23 15:51:02 +0800

    1

--- kada.js
+++ kada.js
@@ -1,6 +1,6 @@
 /*
 [rewrite_local]
-https://m.mallcoo.cn/api/user/user/GetUserAndMallCard url script-response-body https://gitee.com/xubo5200/bonus/raw/master/kada.js
+https://m.mallcoo.cn/api/discount/DiscountCore/Query url script-response-body https://gitee.com/xubo5200/bonus/raw/master/kada.js
 
 https://service.hhdd.com/user/getUserDetail.json
 
@@ -11,36 +11,96 @@ var obj = JSON.parse(body);
 obj={
 	"m" : 1,
 	"d" : {
-	  "IsNotFillFieldForReCreateMallCard" : false,
-	  "UpdateBonus" : 0,
-	  "IsOpenCard" : true,
-	  "IsThirdCRM" : true,
-	  "IsCardAutoUpgrade" : false,
-	  "QrCode" : "FOME000001851052",
-	  "CardTitle" : "银卡",
-	  "IsUnbindMallCard" : false,
-	  "CardUseDesc" : null,
-	  "BarCode" : "FOME000001851052",
-	  "CardTitleUP" : null,
-	  "IsChangeMallCard" : true,
-	  "TotalConsume" : 0,
-	  "NickName" : "wonderful",
-	  "Mobile" : "189***5500",
-	  "SyncErrCode" : 0,
-	  "Bonus" : 150,
-	  "Avatar" : "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJTDVoL8XhVlabMPBhOj5B6XbxySUon2gGTuP2oxNYGhZ9vAXqHdpHA5qwuJMUVJXoT3R3Cx7ibVOg/132",
-	  "IsBindCard" : true,
-	  "CardNo" : "FOME000001851052",
-	  "CardPhoto" : "sp_mall/64/pe/w7/77-5769-47df-83f7-2b6923d8a12b.png",
-	  "IsNeedChooseCard" : false,
-	  "SyncState" : 2,
-	  "CardDegreedeBonus" : 0,
-	  "IsHasPwd" : false,
-	  "TotalBonus" : 150,
-	  "SyncMsg" : null
+	  "PlateNo" : null,
+	  "ParkingTotalFee" : 0,
+	  "IsUseRights" : false,
+	  "CardTitle" : null,
+	  "FreeAmount" : 0,
+	  "Barcode" : null,
+	  "RegisterGuide" : null,
+	  "FeeId" : null,
+	  "ParkingMinutes" : 0,
+	  "UID" : 0,
+	  "NeedPayAmount" : 0,
+	  "ParkingFee" : 0,
+	  "EntryTime" : "0001-01-01 00:00:00",
+	  "Mobile" : null,
+	  "Bonus" : 0,
+	  "ParkName" : null,
+	  "RefID" : null,
+	  "OfflineDeduction" : null,
+	  "WaitPayOrderId" : 0,
+	  "CardNo" : null,
+	  "IsAccessPC" : false,
+	  "MallName" : null,
+	  "IsShowParkZeroPay" : false,
+	  "ParkTel" : null,
+	  "DiscountLimit" : {
+		"MutexList" : null,
+		"UpperList" : null
+	  },
+	  "PaidAmount" : 0,
+	  "DelayTime" : 0,
+	  "RightsRuleModelList" : [
+		{
+		  "IsZeroLimit" : false,
+		  "ZeroType" : 0,
+		  "RightsList" : [
+  
+		  ],
+		  "ListTips" : "无停车券可用",
+		  "NeedMany" : 0,
+		  "DiscountType" : 0,
+		  "IntellegentParams" : "{\"canExceed\":true}",
+		  "RuleName" : "商场停车券",
+		  "ShowType" : 2,
+		  "PanelTips" : "每日可使用1张停车券",
+		  "IntellegentType" : 3,
+		  "SelectedMaxCount" : 1,
+		  "Status" : false,
+		  "IsShow" : true,
+		  "ZeroParams" : "",
+		  "IsIntellegent" : false,
+		  "RuleShowType" : 0,
+		  "Checked" : false
+		},
+		{
+		  "IsZeroLimit" : true,
+		  "ZeroType" : 0,
+		  "RightsList" : [
+			{
+			  "Amount" : 0,
+			  "OrderRecord" : "4",
+			  "RightsID" : "10",
+			  "IsIntellegent" : false,
+			  "Minutes" : 300,
+			  "AllowanceAmount" : 0,
+			  "RightsName" : "10积分抵扣1小时",
+			  "RightsType" : 1,
+			  "RuleShowType" : 0
+			}
+		  ],
+		  "ListTips" : "请选择，10积分可用",
+		  "NeedMany" : 0,
+		  "DiscountType" : 0,
+		  "IntellegentParams" : "",
+		  "RuleName" : "商场积分",
+		  "ShowType" : 1,
+		  "PanelTips" : "每日可用积分抵扣1次，每次最多可使用50积分抵扣",
+		  "IntellegentType" : 2,
+		  "SelectedMaxCount" : 0,
+		  "Status" : true,
+		  "IsShow" : true,
+		  "ZeroParams" : "",
+		  "IsIntellegent" : true,
+		  "RuleShowType" : 0,
+		  "Checked" : false
+		}
+	  ]
 	},
 	"e" : null
   }
+  
   
 body = JSON.stringify(obj);
 $done(body);
