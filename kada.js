599ea62b0c0f0d86f3eede36f223afa0817e55e3
xubo5200 <8413951@qq.com>
2022-02-23 15:30:02 +0800

    1

--- kada.js
+++ kada.js
@@ -1,6 +1,6 @@
 /*
 [rewrite_local]
-^https:\/\/service\.hhdd\.com\/user\/getUserDetail.json url script-response-body https://gitee.com/xubo5200/bonus/raw/master/kada.js
+https://m.mallcoo.cn/api/user/user/GetUserAndMallCard url script-response-body https://gitee.com/xubo5200/bonus/raw/master/kada.js
 
 https://service.hhdd.com/user/getUserDetail.json
 
@@ -9,83 +9,38 @@ https://service.hhdd.com/user/getUserDetail.json
 var body = $response.body;
 var obj = JSON.parse(body);
 obj={
-	"msg": "成功",
-	"data": {
-		"userInfo": {
-			"userId": 40868283,
-			"loginName": "o9bzTww0BfbrPGfM3tQGnMCOQvTc",
-			"gender": "M",
-			"birthday": "2018-08-01",
-			"status": 1,
-			"baseTag": 4102,
-			"level": 10,
-			"inactiveDays": 0,
-			"createDate": 1641824186000,
-			"toast": true,
-			"subscribePlan": false,
-			"collectId": 700001,
-			"schoolPeriod": 0,
-			"ageType": 4,
-			"ageTypeFromBirthday": 4,
-			"isUserSet": 1,
-			"age": 3
-		},
-		"readInfo": {
-			"readNumber": 5,
-			"readTimes": 4885,
-			"honorNumber": 0,
-			"ranking": 0,
-			"allRanking": 0,
-			"storyTime": 3695314,
-			"storyCount": 20,
-			"totalCount": 13
-		},
-		"coinInfo": {
-			"coin": 30,
-			"consumeCoin": 0,
-			"voucherCoin": 30
-		},
-		"vipInfo": {
-			"vipStatus": 0,
-			"beginTime": 0,
-			"endTime": 0,
-			"remindRenewal": 0,
-			"status": 0,
-			"remainingDays": 0,
-			"payFlag": 0,
-			"channel": 0,
-			"withholdingTime": 0,
-			"signStatus": 0,
-			"level": 0,
-			"vipType": 0,
-			"priority": 0
-		},
-		"elfInfo": {
-			"level": 1
-		},
-		"medalList": null,
-		"medalNum": 15,
-		"gameLevel": 1,
-		"currentTime": 1644474737646,
-		"newUserDate": 7,
-		"vipInfoV2": {
-			"vipStatus": 1,
-			"beginTime": 1644474737646,
-			"endTime": 7955821937000,
-			"remindRenewal": 0,
-			"status": 1,
-			"remainingDays": 0,
-			"payFlag": 2,
-			"channel": 2,
-			"withholdingTime": 1644474737646,
-			"signStatus": 2,
-			"level": 5,
-			"vipType": 2,
-			"priority": 0
-		},
-		"vipList": []
+	"m" : 1,
+	"d" : {
+	  "IsNotFillFieldForReCreateMallCard" : false,
+	  "UpdateBonus" : 0,
+	  "IsOpenCard" : true,
+	  "IsThirdCRM" : true,
+	  "IsCardAutoUpgrade" : false,
+	  "QrCode" : "FOME000001851052",
+	  "CardTitle" : "银卡",
+	  "IsUnbindMallCard" : false,
+	  "CardUseDesc" : null,
+	  "BarCode" : "FOME000001851052",
+	  "CardTitleUP" : null,
+	  "IsChangeMallCard" : true,
+	  "TotalConsume" : 0,
+	  "NickName" : "wonderful",
+	  "Mobile" : "189***5500",
+	  "SyncErrCode" : 0,
+	  "Bonus" : 150,
+	  "Avatar" : "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJTDVoL8XhVlabMPBhOj5B6XbxySUon2gGTuP2oxNYGhZ9vAXqHdpHA5qwuJMUVJXoT3R3Cx7ibVOg/132",
+	  "IsBindCard" : true,
+	  "CardNo" : "FOME000001851052",
+	  "CardPhoto" : "sp_mall/64/pe/w7/77-5769-47df-83f7-2b6923d8a12b.png",
+	  "IsNeedChooseCard" : false,
+	  "SyncState" : 2,
+	  "CardDegreedeBonus" : 0,
+	  "IsHasPwd" : false,
+	  "TotalBonus" : 150,
+	  "SyncMsg" : null
 	},
-	"code": 200
-}
+	"e" : null
+  }
+  
 body = JSON.stringify(obj);
 $done(body);
